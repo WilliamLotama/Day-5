1b55d7ca01e566f2c0b8ce7400deea6b88506a4f
william <williamkurniawan10@gmail.com>
2022-03-28 14:03:53 +0700

    update

--- project.js
+++ project.js
@@ -9,15 +9,47 @@ function addProject(event) {
   let startDate = document.getElementById("input-startDate").value
   let endDate = document.getElementById("input-endDate").value
 
-  let html = document.getElementById('input-html').Checked
-  let javaScript = document.getElementById('input-javaScript').Checked
-  let java = document.getElementById('input-java').Checked
-  let android = document.getElementById('input-android').Checked
+  let html = document.getElementById('input-html').checked
+  let javaScript = document.getElementById('input-javaScript').checked
+  let java = document.getElementById('input-java').checked
+  let android = document.getElementById('input-android').checked
   
 
 
   image = URL.createObjectURL(image[0]); // Untuk menampilkan gambar
 
+ 
+
+  console.log(javaScript)
+
+  if(html)
+  {
+    html = document.getElementById('input-html').value;
+  }else{
+    html = " "
+  }
+
+  if(javaScript)
+  {
+    javaScript = document.getElementById('input-javaScript').value;
+  }else{
+    javaScript =" "
+  }
+
+  if(java)
+  {
+    java = document.getElementById('input-java').value;
+  }else{
+    java =" "
+  }
+
+  if(android)
+  {
+    android = document.getElementById('input-android').value;
+  }else{
+    android =" "
+  }
+  
   let project = {
     tittle: tittle,
     desc : desc,
@@ -29,35 +61,6 @@ function addProject(event) {
     java:java,
     android:android
   };
-
-  if(html)
-  {
-    html = document.getElementById('input-html').value
-  }else{
-    html = " "
-  }
-
-  if(javaScript)
-  {
-    javaScript = document.getElementById('input-javaScript').value
-  }else{
-    javaScript =" "
-  }
-
-  if(java)
-  {
-    java = document.getElementById('input-java').value
-  }else{
-    java =" "
-  }
-
-  if(android)
-  {
-    android = document.getElementById('input-android').value
-  }else{
-    android =" "
-  }
-  
 
   projects.push(project); // Menambahkan object project ke dalam array projects
 
@@ -78,14 +81,19 @@ function renderProjects() {
           >
             <h3 id="tittle-card">${projects[dataProject].tittle}</h3>
           </a>
-          <p class="durasi" id="durasi-card">Durasi 3 Bulan</p>
+          <p class="durasi" id="durasi-card">Durasi :
+          ${getDistanceTime(
+            projects[dataProject].startDate,
+            projects[dataProject].endDate
+          )}
+          </p>
 
           <!-- Untuk Desc -->
           <p id="desc-card">
             ${projects[dataProject].desc.slice(0, 30)}
           </p>
 
-          <!-- Icon Code -->
+          <!-- Untuk Halaman Icon -->
           <i class="${projects[dataProject].javaScript}"></i>
           <i class="${projects[dataProject].html}"></i>
           <i class="${projects[dataProject].java}"></i>
@@ -99,3 +107,69 @@ function renderProjects() {
     `;
   }
 }
+
+function getFullTime(timeInput) {
+  let monthName = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
+  // let time = new Date(); // Mengambil waktu saat ini
+
+  let date = timeInput.getDate(); // Untuk Tanggal Kalender
+  let month = timeInput.getMonth(); // Untuk Mengambil Bulan
+  let year = timeInput.getFullYear(); // Untuk Mengambil Tahun
+  let hour = timeInput.getHours(); // Untuk Mengamnil waktu dalam hitungan jam
+  let minute = timeInput.getMinutes(); // Untuk Mengambil waktu dalam hitungan Menit
+
+  let fullTime = `${date} ${monthName[month]} ${year} ${hour}:${minute} WIB`;
+  return fullTime;
+}
+
+// Function untuk menghitung jarak antara dua waktu
+function getDistanceTime(start, end) {
+  let startDate = new Date(start);
+  let endDate = new Date(end);
+
+  let distance = endDate - startDate; // Menghitung jarak waktu antara waktu mulai dan selesai
+  console.log(startDate);
+  let miliseconds = 1000; // 1000 miliseconds dalam 1 detik
+  let secondInHours = 3600; // 1 jam sama dengan 3600 detik
+  let hoursInDay = 24; // 24 jam dalam 1 hari
+  let dayInMonth = 31; // 31 hari dalam 1 bulan
+
+  let distanceMonth = Math.floor(
+    distance / (miliseconds * secondInHours * hoursInDay * dayInMonth) // Untuk menghitung waktu bulan
+  );
+  let distanceDay = Math.floor(
+    distance / (miliseconds * secondInHours * hoursInDay) // Untuk menghitung waktu hari
+  );
+  let distanceHours = Math.floor(distance / (miliseconds * 60 * 60)); // Untuk menghitung waktu jam
+  let distanceMinutes = Math.floor(distance / (miliseconds * 60)); // Untuk menghitung waktu menit
+  let distanceSeconds = Math.floor(distance / miliseconds); // Untuk menghitung waktu detik
+  
+
+  if (distanceMonth > 0) {
+    return `${distanceMonth} bulan`;
+
+  } else if (distanceDay > 0) {
+    return `${distanceDay} hari`;
+  } else if (distanceHours > 0) {
+    return `${distanceHours} jam`;
+  } else if (distanceMinutes > 0) {
+    return `${distanceMinutes} menit`;
+  } else {
+    return `${distanceSeconds} detik`;
+  }
+}
+
